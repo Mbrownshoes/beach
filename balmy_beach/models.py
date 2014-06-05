8dbe8ca5dd51e70025a7686d9c1a385602fe9c35
Mathew Brown <mathew.brown@utoronto.ca>
2014-06-05 10:53:21 -0400

    Added deliberately failing unit test

--- balmy_beach/models.py
+++ balmy_beach/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 from datetime import datetime
 # Create your models here.
-class AirT(models.Model):
-    air_temp = models.IntegerField()
-    def __unicode__(self):
-        return unicode(self.air_temp)
-
 class WaterT(models.Model):
     # airt = models.ForeignKey(AirT)
     surf_temp = models.IntegerField()
     meas_date = models.DateTimeField(default=datetime.now, blank=True)
     def __unicode__(self):
         return unicode(self.surf_temp)
+
+class AirT(models.Model):
+    air_temp = models.IntegerField()
+    watert = models.ForeignKey(WaterT)
+    def __unicode__(self):
+        return unicode(self.air_temp)
 
 class WaterQuality(models.Model):
     ecoli = models.IntegerField()
--- balmy_beach/views.py
+++ balmy_beach/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from balmy_beach.models import AirT, WaterT, WaterQuality
 
 # Create your views here.
+def home(request):
+    airt = AirT.objects.all()
+    return render(request, 'index.html', {'airt': airt})
