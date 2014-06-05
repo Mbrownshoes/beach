c508eb753d79bee0aa31692273ae694ad66b5b6a
Mathew Brown <mathew.brown@utoronto.ca>
2014-06-05 11:55:20 -0400

    added h1 header to index

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,5 +1,5 @@
-from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -11,12 +11,25 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    # def check_for_header_in_table(self, header_text):
+    #     table = self.browser.find_element_by_id('id')
+
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         #Arlo's checking out a new app that shows current water conditions at Balmy beach.
         self.browser.get('http://localhost:8000')
 
         # He notices the header and title mention the beach.
         self.assertIn('Balmy beach',  self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Balmy Beach', header_text)
+
+        # He also sees in large font the air and water temperature and water quality index
+        table = self.browser.find_element_by_id('id_conditions_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('Water temperature', [row.text for row in rows])
+        # At the bottom there is also a 'see past conditions' button, which he presses.
+
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
