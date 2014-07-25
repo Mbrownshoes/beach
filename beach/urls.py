e2de83b917c5084507ae352c2c331320c584b4ca
Mathew Brown <mathew.brown@utoronto.ca>
2014-07-25 09:50:55 -0400

    removed browserid from settings

--- beach/urls.py
+++ beach/urls.py
@@ -6,7 +6,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'balmy_beach.views.home_page', name='home'),
-    url(r'^browserid/', include('django_browserid.urls')),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
